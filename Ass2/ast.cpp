a38d41208429ae7bcdf718fe2653efcf1a87ce8d
Anand Dhoot <anand.dhoot@gmail.com>
2016-02-04 14:19:49 +0530

    Adding lexical analyser

--- Ass2/ast.cpp
+++ Ass2/ast.cpp
@@ -8,13 +8,13 @@ class abstract_astnode
 {
 public:
 virtual void print () = 0;
-virtual std::string generate_code(const symbolTable&) = 0;
-virtual basic_types getType() = 0;
-virtual bool checkTypeofAST() = 0;
+// virtual std::string generate_code(const symbolTable&) = 0;
+// virtual basic_types getType() = 0;
+// virtual bool checkTypeofAST() = 0;
 protected:
-virtual void setType(basic_types) = 0;
+// virtual void setType(basic_types) = 0;
 private:
-typeExp astnode_type;
+// typeExp astnode_type;
 } ;
 
 
---
+++ Ass2/main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+#include "Parser.h"
+using namespace std;
+int main (int argc, char** arg)
+{
+	Parser parser;
+	parser.parse();
+}
