cafae9b49282f844fe8444643e480a032321e115
Anzhit <anchit.post@gmail.com>
2016-02-12 12:14:27 +0530

    Change Problem Statement

--- Assignment02a/test.c
+++ Assignment02a/test.c
@@ -2,14 +2,12 @@ void foo ( )
 {	int s,a,a2;
 	a = 2 ;
 	a=3.23;
-	for (x=0; x <10; x++);
+	for (x=0; x <10; x++){
+
+			for (x=0; x <10; x++){
+				if (y >1) {x=x-1; y=y+1;} else ;
+			}
+	}
 if (y >1) {x=x-1; y=y+1;} else ;
 }
 
-void foo ( ) 
-{
-	a = 2 ;
-	a=3.23;
-	for (x=0; x <10; x++);
-if (y >1) {x=x-1; y=y+1;} else ;
-}
