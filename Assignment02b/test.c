f01c7f1ee0fd031a24eb7111a9808aa0d8baf00c
Anzhit <anchit.post@gmail.com>
2016-03-16 21:58:11 +0530

    Minor Fixes

--- Assignment02b/test.c
+++ Assignment02b/test.c
@@ -1,6 +1,34 @@
-int main()
-{
-  int i;
-  int j;
-  (i+j)++;
+struct s {
+    int a;
+    float b;
+};
+
+int * f(void * a, int * b) {
+    float * c;
+    c = a;
+    b=(b);
+    b = a;
+    return a;
+    return b;
+}
+struct t {
+    int a;
+    int b;
+    float d;
+};
+struct jil {
+    int a;
+    int b;
+    float d;
+};
+int g() {
+    struct s a, *p;
+    struct t * b, **q;
+    void * v;
+    int ti[1];
+    p = &a;
+    q = &b;
+    v = f(*q, &(p->a));
+    (*&ti)[0]=1;
+    return (*(*q)).b;
 }
